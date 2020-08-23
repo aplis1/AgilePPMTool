1583dcd3a4312f645c0626ab13aae8d2591797aa
Silpa Aleti <silpa@Silpas-MBP.fios-router.home>
2020-08-23 16:17:23 -0400

    First Commit

---
+++ PPMToolFullStackJava/ppmtool/src/main/java/com/pad/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.pad.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
